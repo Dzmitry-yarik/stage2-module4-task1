b2a9892893c7decb9489aa36efa598fae8840319
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-07-10 19:04:19 +0300

    Update ThreadSafeSingleton.java

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,5 +1,15 @@
 package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
-    // Write your code here!
+    private static ThreadSafeSingleton thread;
+
+    private ThreadSafeSingleton() {
+    }
+
+    public static synchronized ThreadSafeSingleton getInstance() {
+        if (thread == null) {
+            thread = new ThreadSafeSingleton();
+        }
+        return thread;
+    }
 }
